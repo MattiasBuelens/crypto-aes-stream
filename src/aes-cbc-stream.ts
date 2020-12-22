530d1cf761aa0d5587564cafecaace6575b4b7c2
Mattias Buelens <mattias@buelens.com>
2020-12-22 18:53:47 +0100

    Add some comments

--- src/aes-cbc-stream.ts
+++ src/aes-cbc-stream.ts
@@ -32,26 +32,31 @@ class AesCbcStreamTransformer implements Transformer<Uint8Array, Uint8Array> {
         const usableSize = queueSize - remainderSize;
         console.assert(usableSize % AES_BLOCK_SIZE === 0);
 
-        // Take usable bytes from queue
+        // Take usable bytes from the queue.
         const data = takeBytesFromQueue(this._queue, usableSize);
         this._queueSize = remainderSize;
         console.assert(data.byteLength === usableSize);
 
+        // Encrypt a new empty block, to act as padding.
+        // In CBC, the IV to encrypt or decrypt each block is the ciphertext from the previous block.
         const nextIv = data.subarray(data.byteLength - AES_BLOCK_SIZE);
         console.assert(nextIv.byteLength === AES_BLOCK_SIZE);
-
         const padding = await crypto.subtle.encrypt({
             name: 'AES-CBC',
             iv: nextIv
         }, this._key, new Uint8Array(0));
-        const paddedData = concatUint8Arrays([data, new Uint8Array(padding)]);
 
+        // Decrypt the data, with the new padding block.
+        // Since the plaintext of the padding block is empty, no extra unwanted data will end up in the decrypted result.
+        const paddedData = concatUint8Arrays([data, new Uint8Array(padding)]);
         const plain = await crypto.subtle.decrypt({
             name: 'AES-CBC',
             iv: this._iv
         }, this._key, paddedData);
         console.assert(plain.byteLength === data.byteLength);
 
+        // Update the IV for the next block.
+        // Note that this is the same IV that we used to encrypt the padding block.
         this._iv = nextIv;
 
         controller.enqueue(new Uint8Array(plain));
@@ -59,17 +64,22 @@ class AesCbcStreamTransformer implements Transformer<Uint8Array, Uint8Array> {
 
     async flush(controller: TransformStreamDefaultController<Uint8Array>) {
         if (this._queueSize === 0) {
+            // There was no data to decrypt.
+            // Note that if `transform()` is called at least once, the queue is always non-empty.
             return;
         }
 
+        // Decrypt all remaining data, which must contain proper padding.
         const data = concatUint8Arrays(this._queue);
         const plain = await crypto.subtle.decrypt({
             name: 'AES-CBC',
             iv: this._iv
         }, this._key, data);
 
-        controller.enqueue(new Uint8Array(plain));
+        // Clean up
         this._queue.length = 0;
         this._queueSize = 0;
+
+        controller.enqueue(new Uint8Array(plain));
     }
 }
